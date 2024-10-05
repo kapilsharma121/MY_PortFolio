3856b78deba331752c00a2eb6be3a814dde61239
kapil sharma <panditkrish79@gmail.com>
2024-10-05 11:30:25 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm krish, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          I am parsuinng B.Tech from GL Bajaj Group of institution(Mathura). and, I am also completed  my 12th from G.V.P  School(Goverdhan).
+          I am completed 3 Months offline MERN INternship & get Certificate.
+
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in java, Experienced by full stack  developer, Proficient in javascript , Proficient in React Js, Proficient in Node Js, Proficient in MongoDB, Proficient in Python
+          </span>
+          <br />
+          <br />
+
+    <h1 className="text-green-600 font-semibold text-xl">Profesnal Experience</h1>
+              <span>
+          [Job Title], [Company/Organization], [Dates] [Brief description of
+          responsibilities and achievements] [Job Title],
+          [Company/Organization], [Dates] [Brief description of responsibilities
+          and achievements] [Freelance/Contract Work], [Client/Organization],
+          [Dates] [Brief description of projects and contributions]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+         I have certified by GREAkSOFGREAKS Hackathon.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [Your Field] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
